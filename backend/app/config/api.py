678f76e011232580def836620f7c03eb05a398d3
hafiz-ahtasham-ali <hafizahtasham07@gmail.com>
2025-11-16 16:47:50 +0500

    added backend/logs/ in .gitignore

--- backend/app/config/api.py
+++ backend/app/config/api.py
@@ -1,3 +1,5 @@
 WHATSAPP_API_URL="http://localhost:3000/send-message"
+
 ASSEMBLYAI_BASE_URL = "https://api.assemblyai.com"
+
 OPENAI_API_URL = "https://api.openai.com/v1/chat/completions"
--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -2,6 +2,6 @@ FLASK_ENV = "development" # development or production
 
 OPENAI_CHAT_MODEL = "gpt-5.1"
 
-OPENAI_SYSTEM_PROMPT = "You are a helpful assistant."
+OPENAI_SUMMARIZATION_SYSTEM_PROMPT = "You are a helpful assistant."
 
 DEFAULT_GOAL_EMOJI = "🎯"
