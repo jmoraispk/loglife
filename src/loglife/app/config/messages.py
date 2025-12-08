908320bdd22343355c08dd1d675aef4e55974afe
Hafiz Ahtasham Ali <66234123+hafiz-ahtasham-ali@users.noreply.github.com>
2025-12-08 13:10:58 +0500

    ...

--- src/loglife/app/config/messages.py
+++ src/loglife/app/config/messages.py
@@ -39,7 +39,7 @@ What would you like to start with?"""
 # -----------------------------
 # Help Messages
 # -----------------------------
-HELP_MESSAGE = """hello 123 456 789 ❓ *LogLife Commands*
+HELP_MESSAGE = """❓ *LogLife Commands*
 
 📋 *GOALS*
 • `goals` - Show your personal goals
